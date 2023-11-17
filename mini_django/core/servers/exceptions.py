33d07864c08381acd31388c368ccad17bbaeb700
xuruilong <2996613775@qq.com>
2023-11-17 21:48:18 +0800

    晚上整体服务

---
+++ mini_django/core/servers/exceptions.py
@@ -0,0 +1,10 @@
+"""
+自定义异常
+"""
+
+
+class ImProperlyConfigured(Exception):
+    """
+    自定义的导入模块错误
+    """
+    pass
---
+++ mini_django/manage.py
@@ -0,0 +1,5 @@
+from mini_django.core.managements.commands.runserver import RunServer
+
+if __name__ == '__main__':
+    run_server = RunServer()
+    run_server.run(False, "0", 81)
